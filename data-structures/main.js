68902c46a93f8ea0df482e7dd6d1e965e36da8fb
samithsarasan <samith.sarasan@perfomatix.com>
2017-11-06 15:06:35 +0530

    Implemented stack

---
+++ data-structures/main.js
@@ -0,0 +1,13 @@
+var stack = require('./stack/stack');
+
+
+
+// Stack Operations
+stack.push(2);
+stack.push(3);
+
+console.log(stack.peek());
+
+console.log(stack.pop());
+
+console.log(stack.size());
---
+++ data-structures/stack/stack.js
@@ -0,0 +1,35 @@
+/* Stack */
+var Stack = function () {
+    this.count = 0;
+    this.storage = {};
+
+    // Insert a value into the end of the stack
+    this.push = function (value) {
+        this.storage[this.count] = value;
+        this.count++;
+    }
+
+    // Removes a value from the end and returns that value
+    this.pop = function () {
+        if (this.count === 0) {
+            return undefined;
+        }
+
+        this.count--;
+        var result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+    }
+
+    // Returns the end value in the stack
+    this.peek = function () {
+        return this.storage[this.count - 1];
+    }
+
+    // Returns the number of items in the stack
+    this.size = function () {
+        return this.count;
+    }
+}
+
+module.exports = new Stack();
